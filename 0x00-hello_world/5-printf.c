0b637d1bd4a7bbf03bf40a3b68673c476ec82dbf
may10-codes <simonm487@gmail.com>
2023-02-16 05:42:27 -0800

    size c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+1#include <stdio.h>
+2
+3/**
+4 * main - Entry point
+5 * printing the outcome is a piece of art,
+6 * Return: Always 0 (Success)
+7 */
+8int main(void)
+9{
+10	printf("with proper grammar, but the outcome is a piece of art,\n");
+11	return (0);
+12}
