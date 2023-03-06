989687bb267ddea9b0319468c3858201b13a5814
may10-codes <simonm487@gmail.com>
2023-03-06 02:55:12 -0800

    strch

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,22 @@
 #include "main.h"
+/**
+ * _memeset - a function that fills memory with a constant byte.
+ * fills the first n bytes of the memory area pointed to s with constant b
+ * @b: constant to fill the memory with.
+ * @n: bytes of the memory area to be filled.
+ * @s: pointer to the memory area.
+ *
+ * Return: pointer to the memory of s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-    for (unsigned int i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
-    return s;
-}
+	unsigned int i;
 
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
