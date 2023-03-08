8c75587bdce89ef16a0deabc8de496aee2c13c01
may10-codes <simonm487@gmail.com>
2023-03-08 08:44:22 -0800

    prime numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,34 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Calculates the natural square root of a number.
+ * sqrt_recursion - calculates the natural square root of a number
+ * @n: the number to calculate the square root of
+ * @root: the current guess for the square root of n
  *
- * @n: The number to find the square root of.
+ * Return: the natural square root of n, otherwise -1 if n does not have a
+ *         natural square root
+ */
+int sqrt_recursion(int n, int root)
+{
+	if (root * root == n)
+		return (root);
+	if (root * root > n)
+		return (-1);
+	return (sqrt_recursion(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number
+ * @n: the number to calculate the square root of
  *
- * Return: The square root of the number, -1 if no natural square root.
+ * Return: the natural square root of n, otherwise -1 if n does not have a
+ *         natural square root
  */
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
-	{
+	if (n < 0)
 		return (-1);
-	}
-	else if (n == (int)sq_root(n) * (int)sq_root(n))
-	{
-		return (sqrt(n));
-	}
-	else
-	{
-		return (-1);
-	}
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt_recursion(n, 1));
 }
-
