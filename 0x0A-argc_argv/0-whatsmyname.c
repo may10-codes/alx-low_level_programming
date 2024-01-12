91a4a72a226ff8f6f8017a5cb482d62759fc20f1
may10-codes <simonm487@gmail.com>
2024-01-12 11:35:18 -0800

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - Function that prints its name
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+ 
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int atoi(char *s);
+ 
+#endif
