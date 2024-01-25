48adeb175636c300c7f952663daf07b5e65e9073
may10-codes <simonm487@gmail.com>
2024-01-25 07:52:04 -0800

    prints opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function.
+ * @argc: num of arguments
+ * @argv: array of arguments
+ * Return: 0 always on success
+ */
+
+int main(int argc, char *argv[])
+{
+    int bytes, (i);
+    char *arr;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    int num_bytes = atoi(argv[1]);
+
+    if (num_bytes < 0)
+    {
+        printf("Error\n");
+        return (2);
+    }
+    arr = (char *)main;
+    for (i = 0; i < bytes; i++)
+    {
+        if (i == num_bytes - 1)
+            printf("%02hhx\n", arr[i]);
+    };
+    printf("%02hhx\ , arr[i]");
+
+    return (0);
+}
+
