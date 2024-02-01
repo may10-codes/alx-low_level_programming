262eee4d126083f859698b506a6bebffa23d6649
may10-codes <simonm487@gmail.com>
2024-02-01 10:16:51 -0800

    free node

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Deallocate memory allocated
+ * @head: Linked list
+ */
+void free_list(list_t *head)
+{
+    list_t *curr, *next;
+
+    curr = head;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        free(curr->str);
+        free(curr);
+        curr = next;
+    }
+}
+
