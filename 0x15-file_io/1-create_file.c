f88d81bf7f138b8eb93b9fefb038b3b0616d099a
may10-codes <simonm487@gmail.com>
2024-02-12 11:03:11 -0800

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include"main.h"
+#include<sys/types.h>
+#include<sys/stat.h>
+
+/**
+  * create_file-Createsafile
+  * @filename:Nameoffile
+  * @text_content:Contenttowritetothefile
+  *
+  * Return:1onsuccess,-1onfailure
+  */
+
+int create_file(const char*filename,char*text_content)
+{
+	int fd,num_written,num_bytes=0;
+
+	if(filename==NULL)
+		return(-1);
+
+	fd=open(filename,O_CREAT|O_WRONLY|O_TRUNC,S_IRUSR|S_IWUSR);
+
+	if(fd==-1)
+		return(-1);
+
+	if(text_content==NULL)
+	{
+		close(fd);
+		return(1);
+	}
+
+	while(text_content[num_bytes])
+		num_bytes++;
+
+	num_written=write(fd,text_content,num_bytes);
+	close(fd);
+
+	if(num_written==-1||num_written!=num_bytes)
+		return(-1);
+
+	return(1);
+}
+
